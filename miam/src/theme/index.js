7923c32dcb43fa6fc0a17380d99b9d6985cb1ec9
Francois Lannoy <flannoy154@gmail.com>
2021-03-30 10:02:44 +0200

    :lipstick: added colors const

---
+++ miam/src/theme/index.js
@@ -0,0 +1,10 @@
+export const colors = {
+    primaryGrey: '#212121',
+    grey2: '#272727',
+    grey3: '#393939',
+    grey4: '#4a4a4a',
+    grey5: '#707070',
+    textWhite: '#fff',
+    yellow1: '#F5BF42',
+    yellow2: '#FFE49F',
+}
