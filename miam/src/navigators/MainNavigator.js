4352663643263adfe10912014d1b0550087a5fed
Francois Lannoy <flannoy154@gmail.com>
2021-03-30 09:31:43 +0200

    :wrench: forgot to add gesture handler import
    bruh

--- miam/src/navigators/MainNavigator.js
+++ miam/src/navigators/MainNavigator.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -12,7 +11,6 @@ export default function MainNavigator() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={App} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
