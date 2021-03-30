f0e05ec699edc99ed48f3e679fe798fb76c323fc
Francois Lannoy <flannoy154@gmail.com>
2021-03-31 00:44:57 +0200

    :globe_with_meridians: set up i18n

---
+++ miam/src/components/DurationIndicator/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native'
+import {colors} from '../../theme'
+
+export default StyleSheet.create({
+    container: {
+        backgroundColor: colors.yellow2,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 90,
+        height: 40,
+        borderRadius: 10,
+        
+    },
+    text: {
+        color: colors.grey3,
+    }
+}) 
