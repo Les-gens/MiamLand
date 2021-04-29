dd7aa9da5fc7d6d0c7d302d825783f6eeb103e17
Galimede <mat.degand@outlook.fr>
2021-04-29 10:28:37 +0200

    :sparkles: Add User Model and conf

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,11 @@
 import fastify from 'fastify';
+import sequelize from "./config.js";
+import User from "./models/User.js";
 
-// Instantiate fastify
 const server = fastify({ logger: true });
+
+// /!\ Remove force sync when in prod
+const synchronize = async () => await sequelize.sync({force:true});
 
 // Default route
 server.get('/', async (req, res) => {
@@ -12,11 +16,12 @@ server.get('/', async (req, res) => {
 const PORT = 8000 || process.env.PORT;
 const startServer = async () => {
   try {
-    await server.listen(PORT);
+      await server.listen(PORT);
   } catch (err) {
     server.log.error(err);
     process.exit(1);
   }
 };
-
+synchronize();
 startServer();
+
---
+++ backend/src/models/User.js
@@ -0,0 +1,29 @@
+import  sequelize  from '../config.js';
+import pkg from 'sequelize';
+const { DataTypes } = pkg;
+
+const User = sequelize.define('user', {
+    userID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+    },
+    firstName: {
+        type: DataTypes.STRING,
+    },
+    lastName: {
+        type: DataTypes.STRING
+    },
+    userName: {
+        type: DataTypes.STRING(50)
+    },
+    password: {
+        type: DataTypes.STRING
+    }
+}, {
+    // Other model options go here
+});
+
+console.log(User === sequelize.models.User);
+
+export default User;
