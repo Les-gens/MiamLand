08719393d3173107cce4586a53e0e55e564876c7
Galimede <mat.degand@outlook.fr>
2021-05-18 18:07:02 +0200

    :rotating_light: Fix linter warnings

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,27 +1,27 @@
-import  sequelize  from '../config.js';
+import sequelize from '../config.js';
 import pkg from 'sequelize';
 const { DataTypes } = pkg;
 
 const User = sequelize.define('user', {
-    userID: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true
-    },
-    firstName: {
-        type: DataTypes.STRING,
-    },
-    lastName: {
-        type: DataTypes.STRING
-    },
-    userName: {
-        type: DataTypes.STRING(50)
-    },
-    password: {
-        type: DataTypes.STRING
-    }
+  userID: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true
+  },
+  firstName: {
+    type: DataTypes.STRING
+  },
+  lastName: {
+    type: DataTypes.STRING
+  },
+  userName: {
+    type: DataTypes.STRING(50)
+  },
+  password: {
+    type: DataTypes.STRING
+  }
 }, {
-    // Other model options go here
+  // Other model options go here
 });
 
 console.log(User === sequelize.models.User);
