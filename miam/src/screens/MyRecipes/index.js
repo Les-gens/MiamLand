a0cfcb8b351399436226f72d9cdf3e0fbb876510
Francois Lannoy <flannoy154@gmail.com>
2021-05-23 21:15:48 +0200

    :construction: working on MyRecipes

--- miam/src/screens/MyRecipes/index.js
+++ miam/src/screens/MyRecipes/index.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import { View } from 'react-native'
 
+import { Plus } from '../../assets/index.js'
+import { Recipe } from '../../models'
+import jambon from '../../assets/img/jambon.jpg'
+import { RecipeCard } from '../../components/index'
+import { colors } from '../../theme/index.js'
+import styles from './styles'
+
 const MyRecipes = () => {
+  const list = []
+  // const RECIPE = new Recipe('Endives au jambon', 4, 10, 10, jambon)
+  for (let i = 0; i < 5; i++) {
+    // list.push(<RecipeCard recipe={RECIPE} />)
+    list.push(<View />)
+  }
     return(
-        <View>
-            
+        <View >
+           <Plus width={25} height={25} fill={colors.textWhite} style={styles.plusButton}/> 
         </View>
     )
 }
---
+++ miam/src/screens/MyRecipes/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+
+  },
+  plusButton: {
+    right: 10,
+    top: 10,
+    position: 'absolute',
+  },
+})
