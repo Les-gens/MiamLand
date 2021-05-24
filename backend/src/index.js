45301d30586984f3414402e8b15c0ba14afb7b47
Galimede <mat.degand@outlook.fr>
2021-05-24 17:21:43 +0200

    :sparkles: Add file upload

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,17 +2,20 @@ import fastify from 'fastify';
 import sequelize from './config.js';
 import fastifyMultipart from 'fastify-multipart';
 import fastifyJWT from 'fastify-jwt';
-
+import fs from 'fs';
 import ingredientRoutes from './routes/ingredientRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import fridgeRoutes from './routes/fridgeRoutes.js';
 import recipeRoutes from './routes/recipeRoutes.js';
 import stepRoutes from './routes/stepRoutes.js';
+import fastifyStatic from 'fastify-static';
+import path from 'path';
 
 const baseRoutes = [ingredientRoutes, userRoutes, fridgeRoutes, recipeRoutes, stepRoutes];
 const server = fastify({ logger: true });
 server.register(fastifyMultipart);
 server.register(fastifyJWT, { secret: (process.env.JWT_SECRET || 'imabanana') });
+server.register(fastifyStatic, { root: path.join(path.resolve(), 'uploads'), prefix: '/uploads/' });
 
 // /!\ Remove force sync when in prod
 const synchronize = async () => await sequelize.sync({ force: true });
@@ -20,6 +23,10 @@ const synchronize = async () => await sequelize.sync({ force: true });
 // Default route
 server.get('/', async (req, res) => {
   return { Message: 'Welcome to MiamLand API' };
+});
+
+server.get('/images/:image', async (req, res) => {
+  res.sendFile(req.params.image);
 });
 
 baseRoutes.forEach((routes) => {
