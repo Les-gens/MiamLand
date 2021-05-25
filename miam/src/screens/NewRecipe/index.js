ad05e4d54d83ea3d7ab7f3369189de401886141c
Francois Lannoy <flannoy154@gmail.com>
2021-05-25 09:15:24 +0200

    :construction: WIP newrecipe

--- miam/src/screens/NewRecipe/index.js
+++ miam/src/screens/NewRecipe/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, TextInput, Text } from 'react-native'
+import {useTranslation} from 'react-i18next'
+
+import styles from './styles'
+import { colors } from '../../theme'
 
 const NewRecipe = () => {
-    return(
-      <View>
-            
+  const {t, i18n} = useTranslation()
+  
+  return(
+    <View style={styles.container}>
+      <View style={styles.input_container}>
+        <Text>{t('title')}</Text>
+        <TextInput style={styles.text_input} placeholderTextColor={colors.grey5} placeholder={t('pumpkin_pie')}></TextInput>
       </View>
-    )
+    </View>
+  )
 }
 
 export default NewRecipe
---
+++ miam/src/screens/NewRecipe/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+import { colors } from "../../theme";
+
+export default StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  text_input: {
+    borderBottomColor: colors.grey3,
+    borderBottomWidth: 1,
+    borderRadius: 10,
+    height: 55,
+    color: colors.grey3,
+  },
+  input_container: {
+    width: '80%',
+    marginTop: 10,
+  }
+})
