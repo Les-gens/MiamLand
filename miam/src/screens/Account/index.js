a615f7245f6b333b9debb376dcde467685851ba0
verzelea <arthurverzele@hotmail.com>
2021-05-26 18:05:51 +0200

    Refactor: exemple State

--- miam/src/screens/Account/index.js
+++ miam/src/screens/Account/index.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import { Pressable, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Button, Pressable, Text, View } from 'react-native'
 import styles from './styles'
 import {useNavigation} from '@react-navigation/native'
 
+
+
 const Account = () => {
+    const [count, setCount] = useState(0)
+
     const navigation = useNavigation()
 
     return(
         <>
+            <Text>Vous avez clique {count} fois</Text>
+            <Pressable 
+            onPress={() => setCount(count +1)}
+            style={({ pressed }) => [
+                {
+                    backgroundColor: pressed
+                    ? 'darkgrey'
+                    : 'grey'
+                },
+                styles.button
+            ]}>
+                <Text style={styles.text}>{'Clique ici'}</Text>
+            </Pressable>
+
+            
             <View style={styles.bottomView}>
                 <View>
                     <Pressable 
