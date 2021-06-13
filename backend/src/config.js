568a799983dd143df5371f125857f41b5cad7eef
Francois Lannoy <flannoy154@gmail.com>
2021-06-13 13:37:06 +0200

    :art: removed breking await keyword
    also indent

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,24 +1,23 @@
 import dotenv from 'dotenv';
+import pkg from 'sequelize';
+const { Sequelize } = pkg;
 dotenv.config();
-import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize({
-    host: process.env.DB_HOST,
-    database: process.env.DB,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    dialect:  'postgres'
+  host: process.env.DB_HOST,
+  database: process.env.DB,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  dialect: 'postgres'
 });
-
-
 
 // Test connection to the DB
 try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
+  sequelize.authenticate();
+  console.log('Connection has been established successfully.');
 } catch (error) {
-    console.error('Unable to connect to the database:', error);
+  console.error('Unable to connect to the database:', error);
 }
 
 export default sequelize;
