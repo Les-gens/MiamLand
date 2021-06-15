81972c03626bc1575257e03c063ea7e0e808beab
verzelea <arthurverzele@hotmail.com>
2021-06-15 17:03:11 +0200

    :sparkles: change page Account et essaie Ajout/Delete ingredient

--- miam/src/screens/AddIngredient/index.js
+++ miam/src/screens/AddIngredient/index.js
@@ -1,32 +1,83 @@
-import React, { useState} from 'react'
+import React, { useEffect, useState} from 'react'
 import { Text, View, Pressable, Switch } from 'react-native'
 import styles from './styles'
 import {useNavigation} from '@react-navigation/native'
 import { TextInput } from 'react-native-gesture-handler'
-
+import axios from 'axios'
 
 const AddIngredient = () => {
     const navigation = useNavigation()
     const [isEnabled, setIsEnabled] = useState(false)
     const toggleSwitch = () => setIsEnabled(previousState => !previousState)
-    const [selected, setSelected] = useState(false)
+    const [ingredientState, setIngredient] = useState(false)
 
+    const [userId, setUserId] = useState(false)
 
     const getFridgeId = () => {
-        console.log("fridge id")
+        console.log("fridge id");
+        let fridgeID = -1;
+        //recup id user
+        useEffect(() => {
+            axios.get(`http://10.0.2.2:8000/api/me`)
+                .then(response => {
+                    setUserId(response.data.userID);
+                })
+                .catch(error => {
+                    console.error('There was an error!', error);
+            });
+        },[])
+
+        //recup id frigo avec id user
+        useEffect(() => {
+            axios.get(`http://10.0.2.2:8000/api/fridges/` + userId + `/byUser`)
+                .then(response => {
+                    fridgeID = response.data.fridgeID;
+                })
+                .catch(error => {
+                    console.error('There was an error!', error);
+            });
+        },[])
+        return fridgeID;
     }
 
     const sendIngredient = () => {
         let fId = getFridgeId()
 
-        if(!isEnabled){
-            console.log("add")
-            // do post request whith fridge id
+        if(fId < 0){
+            console.log('fridgeID not found!');
         }else{
-            console.log("delete")
-            // do delete request with fridge id
+            if(!isEnabled)
+            {
+                console.log("add")
+                //envoi ingredient
+                useEffect(() => 
+                {
+                    setIngredient(ingredientState.toLowerCase())
+                    const ingredientPost = { name: {ingredientState}, fridgeID: fId };
+                    axios.post('http://10.0.2.2:8000/api/ingredients', ingredientPost)
+                    .then(response => console.log(response.data))
+                    .catch(error => {
+                        console.error('There was an error!', error);
+                    });
+                },[])
+
+            }else{
+                console.log("delete")
+                //TODO recup id de l'ingredient (premier qui a le m^me libelle)
+                let ingredientId = '';
+
+                //delete ingredient
+                useEffect(() => {
+                    axios.delete('http://10.0.2.2:8000/api/ingredients/' + ingredientId)
+                    .then(() => console.log(response.data))
+                    .catch(error => {
+                        console.error('There was an error!', error);
+                    });
+                }, []);
+            }
         }
     }
+
     return(
         <>
             <View style={styles.container}>
@@ -34,6 +85,7 @@ const AddIngredient = () => {
                 <TextInput style={styles.textInput} 
                 placeholder="Ingrédient (oeuf, lardon, camembert, ...)"
                 placeholderTextColor="#f8f8f8"
+                onChangeText={(newIngredient) => setIngredient({newIngredient})}
                 underlineColorAndroid={'transparent'} />
 
                 <View style={styles.containerVertical}>
