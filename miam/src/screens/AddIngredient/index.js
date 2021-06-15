395d8a9919b42d93d71ce3e6d5256391aab2477f
verzelea <arthurverzele@hotmail.com>
2021-06-15 19:10:59 +0200

    :bug: Erreur asynchrone

--- miam/src/screens/AddIngredient/index.js
+++ miam/src/screens/AddIngredient/index.js
@@ -4,72 +4,77 @@ import styles from './styles'
 import {useNavigation} from '@react-navigation/native'
 import { TextInput } from 'react-native-gesture-handler'
 import axios from 'axios'
+import { add } from 'react-native-reanimated'
 
 const AddIngredient = () => {
     const navigation = useNavigation()
     const [isEnabled, setIsEnabled] = useState(false)
     const toggleSwitch = () => setIsEnabled(previousState => !previousState)
-    const [ingredientState, setIngredient] = useState(false)
-
-    const getFridgeId = () => {
-        console.log("fridge id");
-        let fridgeID = -1;
-        let userID = -1;
-        //recup id user
+    const [ingredientState, setIngredient] = useState('')
+    
+    //recup id user
+    const getFridgeId = () => {        
         axios.get(`http://10.0.2.2:8000/api/me`)
             .then(response => {
-                console.log('step 1: '+response.data[0].userID)
-                userID = response.data[0].userID;
+                getFridgeIdWithUser(response.data[0].userID)
             })
             .catch(error => {
                 console.error('There was an error!', error);
         });
-        //recup id frigo avec id user
+        return -1;
+    }
+    
+    //recup id frigo avec id user
+    const getFridgeIdWithUser = (userID) => {
         axios.get(`http://10.0.2.2:8000/api/fridges/` + userID + `/byUser`)
             .then(response => {
-                console.log('step 2: '+response.data[0].fridgeID)
-                fridgeID = response.data[0].fridgeID;
+                let fId = response.data[0].fridgeID
+                if(fId < 0){
+                    console.log('fridgeID not found!');
+                }else{
+                    if(!isEnabled)
+                    {
+                        add(fId)
+                    }else{
+                        remove(fId)
+                    }
+                }
             })
             .catch(error => {
                 console.error('There was an error!', error);
         });
-        console.log('step 3: '+fridgeID)
-        return fridgeID;
+    }
+
+    //ajout
+    const add = (fId) => {
+        console.log('add')
+        setIngredient(ingredientState.toLowerCase())//TODO erreur avec tolowercase
+        const ingredientPost = { name: {ingredientState}, fridgeID: fId };
+        axios.post('http://10.0.2.2:8000/api/ingredients', ingredientPost)
+            .then(response => console.log(response.data[0]))
+            .catch(error => {
+                console.error('There was an error!', error);
+        });
+    }
+
+    //retrait
+    const remove = (fId) => {
+        console.log('remove')
+        //TODO recup id de l'ingredient (premier qui a le m^me libelle)
+        
+    }
+
+    //retrait avec ingredient
+    const removeWithIngredient = (fId) => {
+        axios.delete('http://10.0.2.2:8000/api/ingredients/' + ingredientId)
+        .then(() => console.log(response.data[0]))
+        .catch(error => {
+            console.error('There was an error!', error);
+        });
     }
 
     const sendIngredient = () => {
-        let fId = getFridgeId()
-        console.log('step 4: '+fId)
-        if(fId < 0){
-            console.log('fridgeID not found!');
-        }else{
-            if(!isEnabled)
-            {
-                console.log("add")
-                //envoi ingredient
-                setIngredient(ingredientState.toLowerCase())
-                const ingredientPost = { name: {ingredientState}, fridgeID: fId };
-                axios.post('http://10.0.2.2:8000/api/ingredients', ingredientPost)
-                .then(response => console.log(response.data[0]))
-                .catch(error => {
-                    console.error('There was an error!', error);
-                });
-
-            }else{
-                console.log("delete")
-                //TODO recup id de l'ingredient (premier qui a le m^me libelle)
-                let ingredientId = '';
-
-                //delete ingredient
-                useEffect(() => {
-                    axios.delete('http://10.0.2.2:8000/api/ingredients/' + ingredientId)
-                    .then(() => console.log(response.data[0]))
-                    .catch(error => {
-                        console.error('There was an error!', error);
-                    });
-                }, []);
-            }
-        }
+        getFridgeId()
     }
 
     return(
